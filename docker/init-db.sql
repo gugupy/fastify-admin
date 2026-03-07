2e3fc72656ce27ed5d24e9fd9a9a98504e6ac2ec
gugupy <gugu.ap900@gmail.com>
2026-03-07 15:38:00 +0530

    Remove init-db.sql and docker mount

--- docker/init-db.sql
+++
@@ -1,2 +0,0 @@
-SELECT 'CREATE DATABASE fastifyadmin' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fastifyadmin')\gexec
-SELECT 'CREATE DATABASE fastifyadmin_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fastifyadmin_test')\gexec
